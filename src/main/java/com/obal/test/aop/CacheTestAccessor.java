7effd9f126c4c61f47e5cd11ac188a8f0c05cc7a
despird-zh <despird.zhang@yahoo.com>
2014-11-06 22:23:57 +0800

    aop testing

---
+++ src/main/java/com/obal/test/aop/CacheTestAccessor.java
@@ -0,0 +1,9 @@
+package com.obal.test.aop;
+
+public class CacheTestAccessor {
+	
+	public void doPutDemo1(){
+		
+		System.out.println("--------calling doGetDemo1");
+	}
+}
---
+++ src/main/java/com/obal/test/aop/TestMain.java
@@ -0,0 +1,11 @@
+package com.obal.test.aop;
+
+public class TestMain {
+
+	public static void main(String[] ars){
+		
+		CacheTestAccessor cta = new CacheTestAccessor();
+		
+		cta.doPutDemo1();
+	}
+}
