e8caaa9f7ebec08f829faf8be8e9be4f80a6e0f8
despird-zh <despird.zhang@yahoo.com>
2014-11-08 13:44:49 +0800

    cache aspect

---
+++ src/main/java/com/obal/cache/aop/CacheablePut.java
@@ -0,0 +1,50 @@
+package com.obal.cache.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import com.obal.core.util.CoreConstants;
+
+/**
+ * Cacheable indicate whether the method parameter or return value need cache operation
+ * <p>
+ * Cache operation usually occurs on two kinds of situation: entry or attribute.
+ * as per store decide where to r/w cache.
+ * </p>
+ * <p>Entry value cache: need to specify necessary( entrykey+entity+value / entrykey+value )</p>
+ * <p>Attribute value cache: need to specify necessary( entrykey+attr+value )</p>
+ * 
+ * @author despird
+ * @version 0.1 2014-2-1
+ * 
+ * @See EntryKey
+ **/
+@Target(ElementType.METHOD)
+public @interface CacheablePut {
+
+	/**
+	 * decide where to store the cache
+	 * ie. it's the accessor builder name: redis or hbase 
+	 **/
+	String store() default CoreConstants.BUILDER_REDIS;
+	
+	/**
+	 * the parameter name of EntryKey object or String value of key
+	 **/
+	String entrykey() default "";
+	
+	/**
+	 * the parameter name indicate the entity name
+	 **/
+	String entity() default "";
+	
+	/**
+	 * the parameter name of attribute object or string name of attribute 
+	 **/
+	String attr() default "";
+	
+	/**
+	 * the name of object to be applied cache operation
+	 **/
+	String cache() default "";
+}
