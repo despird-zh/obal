8114ebdb4ce8f883b6fae49535e2bc5617900c09
despird-zh <despird.zhang@yahoo.com>
2014-11-22 14:57:32 +0800

    update

---
+++ src/main/java/com/obal/test/redis/RedisTester.java
@@ -0,0 +1,16 @@
+package com.obal.test.redis;
+
+import com.obal.test.AdminTester;
+import com.obal.test.BlankTester;
+
+public class RedisTester extends BlankTester{
+
+	public static void main(String[] args){
+		
+		initLog4j();
+		RedisTester tester = new RedisTester();
+	
+	
+
+	}
+}
