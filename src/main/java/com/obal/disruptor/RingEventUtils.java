c3bd56c3e49f54d4937bd30c3238bc55ebf025d2
despird-zh <despird.zhang@yahoo.com>
2014-12-05 18:49:56 +0800

    up

--- src/main/java/com/obal/disruptor/RingEventUtils.java
+++ src/main/java/com/obal/disruptor/RingEventUtils.java
@@ -6,4 +6,19 @@ public class RingEventUtils {
 		
 		EventDispatcher.getInstance().publishPayload(payload);
 	}
+	
+	public static void regEventHooker(EventHooker eventHooker){
+		
+		EventDispatcher.getInstance().regEventHooker(eventHooker);
+	}
+	
+	public static void unRegEventHooker(EventType type){
+		
+		EventDispatcher.getInstance().unRegEventHooker(type);
+	}
+	
+	public static void blockEventHooker(EventType type, boolean blocked){
+		
+		EventDispatcher.getInstance().blockEventHooker(type, blocked);
+	}
 }
