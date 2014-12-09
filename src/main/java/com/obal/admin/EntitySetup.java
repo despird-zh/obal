90ece4b5d5a6273fd0ee1cf241b8a8020970e163
despird-zh <despird.zhang@yahoo.com>
2014-12-09 14:30:28 +0800

    up

--- src/main/java/com/obal/admin/EntitySetup.java
+++ src/main/java/com/obal/admin/EntitySetup.java
@@ -77,11 +77,9 @@ public class EntitySetup {
 		meta.addAttr(attr);
 		attr = new EntityAttr("i_lastmodify", AttrType.DATE, "c0", "lastmodify");
 		meta.addAttr(attr);
-		attr = new EntityAttr("i_group", AttrMode.MAP, AttrType.STRING, "c1",
-				"group");
+		attr = new EntityAttr("i_group", AttrMode.MAP, AttrType.STRING, "c1", "group");
 		meta.addAttr(attr);
-		attr = new EntityAttr("i_role", AttrMode.MAP, AttrType.STRING, "c1",
-				"role");
+		attr = new EntityAttr("i_role", AttrMode.MAP, AttrType.STRING, "c1", "role");
 		meta.addAttr(attr);
 		ea.setupSchema(meta);
 
--- src/main/java/com/obal/core/hbase/HMapRedScan.java
+++ src/main/java/com/obal/core/hbase/HMapRedScan.java
@@ -19,22 +19,28 @@
  */
 package com.obal.core.hbase;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.client.Put;  
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Result;  
 import org.apache.hadoop.hbase.client.Scan;  
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;  
+import org.apache.hadoop.hbase.mapreduce.ResultSerialization;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;  
 import org.apache.hadoop.hbase.mapreduce.TableMapper;  
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.io.serializer.Serializer;
 import org.apache.hadoop.mapreduce.Job;  
+import org.apache.log4j.PropertyConfigurator;
 
-import com.obal.core.util.AdminUtils;
-import com.obal.exception.AccessorException;
-import com.obal.meta.EntityAttr;
-
+import java.io.File;
 import java.io.IOException;  
-import java.util.List;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.Properties;
   
 /** 
  * HBase is used as data source as well as data sink. This MapReduce job will try to copy data from 
@@ -46,13 +52,11 @@ import java.util.List;
  */  
 public class HMapRedScan{  
   
+	private Configuration config = null;
+	
 	private String source;
-	
-	private String target;
-	
+		
 	private Scan scan;
-	
-	private List<EntityAttr> attrs;
 	
 	public HMapRedScan(String source,Scan scan){
 		
@@ -62,24 +66,13 @@ public class HMapRedScan{
 		
 	}
 	
-	public void setTarget(String target, List<EntityAttr> attrs){
-		this.attrs = attrs;
-		this.target = target;
-		try {
-			AdminUtils.createSchema(target, attrs);
-		} catch (AccessorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
     public int scan() throws Exception {  
         
-        Job job = Job.getInstance();  
+        Job job = Job.getInstance(config);
         Configuration conf = job.getConfiguration();
         conf.set("mrscan.limit", "20000");
         conf.set("mrscan.principal", "demouser");
-        job.setJobName("HMRScan");
+        job.setJobName("ObalTestScan");
         job.setJarByClass(HMapRedScan.class);  
  
         TableMapReduceUtil.initTableMapperJob(                  
@@ -90,39 +83,124 @@ public class HMapRedScan{
                 null,   // mapper output value  
                 job);  
         TableMapReduceUtil.initTableReducerJob(                 
-                target,  // output table                   
-                null,  // reducer class  
-                job);  
+                "obal.garbage",  // output table                   
+                ScanReducer.class,  // reducer class  
+                job); 
         
         job.setNumReduceTasks(0);// No reducer actually needed here  
   
-        return job.waitForCompletion(true) ? 0 : 1;
-        
+        return job.waitForCompletion(true) ? 0 : 1;        
         
     }  
   
-    public static class ScanMapper extends TableMapper<ImmutableBytesWritable, Put> {  
+    public static class ScanMapper extends TableMapper<ImmutableBytesWritable, Result> {  
     	
     	String principal = null;
     	
-    	protected void setup(Context context) throws IOException,
-    	    InterruptedException {
+    	protected void setup(Context context) throws IOException, InterruptedException {
     		principal = context.getConfiguration().get("mrscan.principal");
     	}
     	 
         public void map(ImmutableBytesWritable row, Result value, Context context)  
                 throws IOException, InterruptedException {  
-  
-            // this example is just copying the data from the source table...  
-            context.write(row, resultToPut(row, value));  
-        }  
-  
-        private static Put resultToPut(ImmutableBytesWritable key, Result result) throws IOException {  
-            Put put = new Put(key.get());  
-            for (Cell kv : result.listCells()) {  
-                put.add(kv);  
-            }
-            return put;  
-        }  
-    }  
+        	System.out.println("Row:"+row);
+            context.write(row, value);  
+        }
+   
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+        	// Nothing
+        }
+    }
+    
+    public static class ScanReducer extends TableReducer<ImmutableBytesWritable, Result, ImmutableBytesWritable> {
+    	
+    	Serializer<Result> serializer;
+    	String feedback = "__DONE__";
+    	HttpClient httpClient = null;
+    	PostMethod post = null;
+    	
+    	protected void setup(Context context) throws IOException, InterruptedException {
+    		ResultSerialization rstSerialUtil = new ResultSerialization();
+        	serializer = rstSerialUtil.getSerializer(Result.class);
+        	httpClient = new HttpClient();
+        	httpClient.getHostConfiguration().setHost("192.168.1.154", 8080, "http");
+        	post = new PostMethod("/demo/mapredresult/");
+    	}
+       
+    	public void reduce(ImmutableBytesWritable key, Iterable<Result> values, Context context)
+                throws IOException, InterruptedException {
+       
+            for (Result val : values) {
+//                PipedOutputStream out = new PipedOutputStream();
+//                serializer.open(out);
+//            	serializer.serialize(val);
+//            	sendToClient(out);
+            	System.out.println("----------"+key);
+            }            
+        }
+        
+        private void sendToClient(PipedOutputStream out){
+        	
+        	PipedInputStream in = new PipedInputStream();
+        	try {
+				out.connect(in);
+				RequestEntity entity=new InputStreamRequestEntity(in);
+	            post.setRequestEntity(entity);
+	             // execute the method
+	            httpClient.executeMethod(post);
+	            feedback = post.getResponseBodyAsString();	           
+	            System.out.println("-- feedback:" + feedback);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+        }
+        
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+        	serializer = null;
+        	httpClient = null;
+        	post = null;
+        }
+    }
+    
+    public void init(){
+    	
+		config = HBaseConfiguration.create();
+		config.set("hbase.zookeeper.property.clientPort", "2181");
+		config.set("hbase.zookeeper.quorum", "192.168.1.133");
+		config.set("hbase.master", "192.168.1.133:60010");
+		File file = new File(".");
+		try {
+
+			String path = file.getCanonicalPath();
+			System.out.println("===:" + path);
+			System.setProperty("hadoop.home.dir", "D:\\n.repo\\hadoop-2.2.0");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public static void main(String[] args) {
+		Properties prop = new Properties();
+
+		prop.setProperty("log4j.rootCategory", "DEBUG, CONSOLE");
+		prop.setProperty("log4j.appender.CONSOLE", "org.apache.log4j.ConsoleAppender");
+		prop.setProperty("log4j.appender.CONSOLE.layout", "org.apache.log4j.PatternLayout");
+		prop.setProperty("log4j.appender.CONSOLE.layout.ConversionPattern", "%d{HH:mm:ss,SSS} [%t] %-5p %C{1} : %m%n");
+		
+		PropertyConfigurator.configure(prop);
+    	String source = "obal.meta.attr";
+    	Scan scan = new Scan();
+    	scan.addFamily("c0".getBytes());
+    	HMapRedScan hmrScan = new HMapRedScan(source, scan);
+    	hmrScan.init();
+    	try {
+			hmrScan.scan();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 }  
