1481fe58ac20be2a671a2172bb73336b26047fa5
despird-zh <despird.zhang@yahoo.com>
2014-12-13 18:31:59 +0800

    up

--- src/main/java/com/obal/core/security/accessor/IUserAccessor.java
+++
@@ -1,8 +0,0 @@
-package com.obal.core.security.accessor;
-
-import com.obal.core.IEntityAccessor;
-import com.obal.core.accessor.RawEntry;
-
-public interface IUserAccessor extends IEntityAccessor<RawEntry>{
-	
-}
--- src/main/java/com/obal/core/security/hbase/UserAccessor.java
+++ src/main/java/com/obal/core/security/hbase/UserAccessor.java
@@ -3,10 +3,9 @@ package com.obal.core.security.hbase;
 import com.obal.core.hbase.HEntryWrapper;
 import com.obal.core.accessor.RawEntry;
 import com.obal.core.hbase.HEntityAccessor;
-import com.obal.core.security.accessor.IUserAccessor;
 import com.obal.meta.BaseEntity;
 
-public class UserAccessor extends HEntityAccessor<RawEntry> implements IUserAccessor {
+public class UserAccessor extends HEntityAccessor<RawEntry> {
 
 	public UserAccessor(BaseEntity schema) {
 		super(schema);
